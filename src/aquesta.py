a5c1bf56b9a04b476d9ab71efe80c9bb31045dab
Bernardo Heynemann <heynemann@gmail.com>
2012-02-14 10:50:49 -0200

    Fib

--- src/aquesta.py
+++ src/aquesta.py
@@ -1,7 +1,17 @@
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n - 1) + fib (n - 2)
+
 def main(argv):
-    print "hello world"
+    print fib(int(argv[1]))
 
     return 0
 
 def target(driver, args):
     return main, None
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
