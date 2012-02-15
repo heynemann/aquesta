521cbafe968a04622a8e452ba934ef6cef0f6df7
Bernardo Heynemann <heynemann@gmail.com>
2012-02-14 23:53:57 -0200

    grammar began

--- src/aquesta.py
+++ src/aquesta.py
@@ -1,17 +1,33 @@
-def fib(n):
-    if n <= 2:
-        return 1
-    else:
-        return fib(n - 1) + fib (n - 2)
+from pypy.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
+from pypy.rlib.parsing.parsing import ParseError
+from pypy.rlib.streamio import open_file_as_stream
+from os.path import abspath, join, dirname
+import sys
+
+
+def parse(code):
+    GFILE = open_file_as_stream(abspath(join(dirname(__file__), "grammar.txt")))
+
+    t = None
+    try:
+        t = GFILE.read()
+        regexs, rules, ToAST = parse_ebnf(t)
+    except ParseError,e:
+        print e.nice_error_message(filename=str(GFILE),source=t)
+        raise
+
+    parsef = make_parse_function(regexs, rules, eof=True)
+
+    t = parsef(code)
+    return ToAST().transform(t)
 
 def main(argv):
-    print fib(int(argv[1]))
-
+    code = parse(argv[1])
+    print code
     return 0
 
 def target(driver, args):
     return main, None
 
 if __name__ == '__main__':
-    import sys
     main(sys.argv)
